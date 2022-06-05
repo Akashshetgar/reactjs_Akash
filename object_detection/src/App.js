05d5561afe3f29138377e30b73475ea437240592
Akashshetgar <akash.shetgar@somaiya.edu>
2022-06-05 23:04:11 +0530

    basic

---
+++ object_detection/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className="navbar"></div>
+
+      <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js" type="text/javascript"></script>
+      <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd"></script>
+    </div>
+  );
+}
+
+export default App;
